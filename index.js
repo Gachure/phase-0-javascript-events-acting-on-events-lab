ebb0fd24e87177e5e954e8c5950e9a3d6284ad71
victor <victor.gachure@student.moringaschool>
2024-04-02 22:28:27 +0300

    done lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+function moveDodgerLeft() {
+    var currentLeft = parseInt(dodger.style.left || 0);
+    dodger.style.left = (currentLeft - 5) + 'px';
+  }
+  function moveDodgerRight() {
+    var currentLeft = parseInt(dodger.style.left || 0);
+    dodger.style.left = (currentLeft + 5) + 'px';
+  }
